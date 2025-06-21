00b056b18a1e967d7bd3d4c848cfc2e97e4b100c
WongWeiFong <weifong041623@gmail.com>
2025-06-21 21:25:42 +0800

    [dev] update maps & showing states details

--- welcome.php
+++ welcome.php
@@ -10,7 +10,8 @@
 <body>
     <h1>Welcome, <?php echo htmlspecialchars($name); ?>!</h1>
   <p>Let's begin your journey through Malaysia's cultural heritage!</p>
-  <a href="maps-leaflet.html">Heritage Map</a>
+  <a href="maps-google-modern.html">Malaysia Heritage Map (Google)</a>
+  <a href="maps-leaflet.html">Heritage Map (Alternative)</a>
   <a href="profile.php?name=<?php echo urlencode($name); ?>">Profile</a>
   <a href="logout.php">Logout</a>
 </body>
